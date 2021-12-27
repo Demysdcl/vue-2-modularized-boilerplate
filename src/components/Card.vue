22ae9d1e60e088bc1c017920cbb10941e00dd081
Demys Cota <demys.cota@snowmanlabs.com>
2021-12-27 17:09:47 -0300

    chore: generator to components

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Card',
+  components: {},
+  data: () => ({}),
+  computed: {},
+  methods: {},
+  mounted: {},
+}
+</script>
+
+<template>
+  <div class="card"></div>
+</template>
+
+<style lang="scss">
+.card {
+}
+</style>
